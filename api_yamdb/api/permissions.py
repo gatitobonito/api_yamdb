7cf3bcc87b59008490438d6d017ababe6b178d15
Дмитрий Тепикин <dukelouis@yandex.ru>
2022-12-15 11:54:39 +0300

    42 tests. исправил пермишн

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -14,8 +14,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
                 request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+                or (request.user.is_authenticated
+                    and request.user.role == "admin")
         )
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.role == "admin"
+    # def has_object_permission(self, request, view, obj):
+    #     return request.user.role == "admin"

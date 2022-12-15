4c47a606569a78a7719f912c63ac7f28ec081d58
Дмитрий Тепикин <dukelouis@yandex.ru>
2022-12-15 17:01:48 +0300

    50 тестов

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.core.validators import (MaxValueValidator, MinValueValidator,
+                                    RegexValidator)
 ROLES = (
     ('user', 'Пользователь'),
     ('moderator', 'Модератор'),
@@ -13,6 +14,7 @@ ROLES = (
 # )
 
 class User(AbstractUser):
+    username_validator = RegexValidator(r'^[\w.@+-]+')
     bio = models.TextField(
         blank=True,
     )
@@ -22,7 +24,9 @@ class User(AbstractUser):
         default='user'
     )
     confirmation_code = models.CharField(max_length=60, blank=True)
-
+    username = models.CharField(max_length=150, unique=True,
+                                validators=[username_validator])
+    email = models.EmailField(max_length=254, unique=True, blank=False)
     @property
     def is_admin(self):
         return self.role == 'admin' or self.is_staff

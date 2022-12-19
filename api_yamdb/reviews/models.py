09880b429ac122ba3b15c6602fc88538397e1861
Виктория Семеркова <privateadress@yandex.ru>
2022-12-19 22:23:02 +0300

    Add existing file

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from django.db import models
 from django.core.validators import MaxValueValidator, RegexValidator
+from django.core.validators import MinValueValidator
+from django.db.models import Avg
 
 from users.models import User
 
@@ -79,13 +81,18 @@ class Title(models.Model):
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
 
+    def average_rating(self):
+        return self.reviews.aggregate(Avg('score'))['rating']
+
     def __str__(self):
         return self.name
 
 
 class TitleGenre(models.Model):
-    title_id = models.ForeignKey(Title, db_column='title_id', on_delete=models.CASCADE)
-    genre_id = models.ForeignKey(Genre, db_column='genre_id',  on_delete=models.CASCADE)
+    title_id = models.ForeignKey(
+        Title, db_column='title_id', on_delete=models.CASCADE)
+    genre_id = models.ForeignKey(
+        Genre, db_column='genre_id', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title_id} {self.genre_id}'
@@ -94,25 +101,37 @@ class TitleGenre(models.Model):
 class Review(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews')
-    title_id = models.ForeignKey(
+    title = models.ForeignKey(
         Title, on_delete=models.CASCADE, related_name='reviews')
-    score = models.IntegerField(range(1, 11))
+    score = models.IntegerField(
+        validators=[MaxValueValidator(10),
+                    MinValueValidator(1)]
+    )
     text = models.TextField()
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_title_author'
+            )
+        ]
+
     def __str__(self):
-        return f'{self.title_id}, оценка: {self.score}'
+        return f'{self.title}, оценка: {self.score}'
+
 
 class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
     review_id = models.ForeignKey(
-        Review, db_column='review_id', on_delete=models.CASCADE, related_name='comments')
+        Review, db_column='review_id', on_delete=models.CASCADE,
+        related_name='comments')
     text = models.TextField()
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
 
     def __str__(self):
         return f'автор: {self.author}, коммент: {self.text}'
-

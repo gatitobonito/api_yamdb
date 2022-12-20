01a2346c08a69798a15d89abe951c7f695da64d9
Дмитрий Тепикин <dukelouis@yandex.ru>
2022-12-20 15:39:56 +0300

    pagination fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 from datetime import timedelta
 from django.conf.global_settings import DATETIME_INPUT_FORMATS
 
@@ -100,7 +100,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
 }
 
 SIMPLE_JWT = {

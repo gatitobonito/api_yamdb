0019fdb2a4277249d248e7599f1709b38e448694
Дмитрий Тепикин <dukelouis@yandex.ru>
2022-12-20 11:03:22 +0300

    fix pagination flake8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -126,6 +126,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAdminModeratorAuthorOrReadOnly]
     queryset = Comment.objects.all()
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         review_id = get_object_or_404(Review, id=self.kwargs.get('review_id'))
@@ -136,6 +137,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = [IsAdminModeratorAuthorOrReadOnly]
     queryset = Review.objects.all()
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
